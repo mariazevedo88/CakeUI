2dafecbe7f3ff02dc4dcdb97b7d3adc8d737d688
Sarah <caixeta.sarah@gmail.com>
2013-10-21 13:14:45 -0200

    Implementacao de metodos do fragment generico.

--- src/com/cakeui/generic/fragment/GenericFragment.java
+++ src/com/cakeui/generic/fragment/GenericFragment.java
@@ -1,5 +1,32 @@
 package com.cakeui.generic.fragment;
 
-public class GenericFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+public class GenericFragment extends SherlockFragment{
+	
+	private int layoutID;
+	
+	public GenericFragment(){}
+	
+	public GenericFragment(int layoutID){
+		this.layoutID = layoutID;
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		if (layoutID != 0)
+			return inflater.inflate(layoutID, container, false);
+		
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+	
+	
 
 }
