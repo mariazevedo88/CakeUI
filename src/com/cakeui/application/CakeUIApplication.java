a3c06d76c1d6d66fc32c094a54f4c0cd3d1a5213
Mariana Azevedo <mariana.azevedo@mitahtech.com>
2013-11-08 14:31:36 -0200

    Creating the application class and the network connectivity verification method.

--- src/com/cakeui/application/CakeUIApplication.java
+++ src/com/cakeui/application/CakeUIApplication.java
@@ -1,7 +1,39 @@
 package com.cakeui.application;
 
 import android.app.Application;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 public class CakeUIApplication extends Application{
 
+	private static CakeUIApplication instance;
+	private ConnectivityManager connectivityManager; 
+	
+	public CakeUIApplication(){
+		super();
+		if (instance == null)
+			instance = this;
+	}
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+	}
+	
+	public boolean isConnectedToNetwork(){
+		
+		if (connectivityManager == null)
+			return false;
+		
+		NetworkInfo mobileInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+		NetworkInfo wifiInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		if ((mobileInfo != null && mobileInfo.getState() == NetworkInfo.State.CONNECTED)
+				|| (wifiInfo != null && wifiInfo.getState() == NetworkInfo.State.CONNECTED)) {
+			return true;
+
+		}
+		return false; 
+	}
+	
 }
