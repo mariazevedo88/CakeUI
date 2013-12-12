a20e924b200a87f4d64367cdb4da57af3e35fb99
Sarah <caixeta.sarah@gmail.com>
2013-12-12 15:43:12 -0200

    Defined a default value for the time interval of the CakePeriodicService

--- src/com/cakeui/generic/service/CakePeriodicService.java
+++ src/com/cakeui/generic/service/CakePeriodicService.java
@@ -3,6 +3,7 @@ package com.cakeui.generic.service;
 
 /**
  * Class that implements a generic service that is executed periodically.
+ * Initially, is set to execute the task once per minute, but the time interval can be changed.
  * 
  * @author Sarah Caixeta
  * @email caixeta.sarah@gmail.com
@@ -10,8 +11,10 @@ package com.cakeui.generic.service;
  */
 public class CakePeriodicService extends CakeService implements Runnable{
 
+	private static final long ONE_MINUTE = 60000;
+	
 	private Thread syncThread;
-	private long interval;
+	private long interval = ONE_MINUTE;
 	
 	@Override
 	public void onCreate() {
